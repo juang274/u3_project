553ef4f5cfe18ac7ca4cfee5b64d68a0909f21ca
juang274 <hoangnguyen4459@gmail.com>
2023-11-13 17:16:25 -0800

    added all nba logos into the json file

--- u3project/src/components/Team.jsx
+++ u3project/src/components/Team.jsx
@@ -4,7 +4,7 @@ const Team = (props) => {
         <div className ='display'>
         {props.basketball.map((nba) =>(
             <ul key={nba.teamId}>
-                <img src={nba.logo} alt="" height='400' width='400'/>
+                <img src={nba.logo} alt="" height='300' width='300'/>
                 <h2>{nba.teamName}</h2>
                 <h3>{nba.abbreviation}</h3>   
             </ul>    
