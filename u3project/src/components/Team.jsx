edec9b28ecf02317398fb2085c60fabac5aed241
juang274 <hoangnguyen4459@gmail.com>
2023-11-15 03:15:09 -0800

    add image change when hovering an NBA logo

--- u3project/src/components/Team.jsx
+++ u3project/src/components/Team.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import TeamColors from "./TeamColors"
 
 
@@ -7,14 +8,15 @@ import TeamColors from "./TeamColors"
 
 const Team = (props) => {
 
-
     return(
 
-        
         <div className ='display'>
         {props.basketball.map((nba) =>(
             <ul key={nba.teamId}>
-                <img src={nba.logo} alt="" height='300' width='300'/>
+                <img className='normal' src={nba.logo} 
+                onMouseEnter ={e => (e.currentTarget.src = "https://upload.wikimedia.org/wikipedia/commons/f/f1/Steph_Curry_P20230117AS-1347_%28cropped%29.jpg")}
+                onMouseLeave={e=> (e.currentTarget.src = nba.logo)} 
+                alt="" height='300' width='300'/>
                 <h2>{nba.teamName}</h2>
                 <TeamColors abbrev={nba.abbreviation}/>
             </ul>
