6623ded3df3a49fef81b2bd31ec138e1bd5c0bd5
juang274 <hoangnguyen4459@gmail.com>
2023-11-15 17:11:44 -0800

    nba logo gets bigger when hovered

--- u3project/src/components/NBALogo.jsx
+++ u3project/src/components/NBALogo.jsx
@@ -1,6 +1,6 @@
 function NBALogo(){
     return(
-        <img src="https://images.ctfassets.net/h8q6lxmb5akt/5qXnOINbPrHKXWa42m6NOa/421ab176b501f5bdae71290a8002545c/nba-logo_2x.png" alt="" height="400" width= "600"/>
+        <img className="nbazoom" src="https://images.ctfassets.net/h8q6lxmb5akt/5qXnOINbPrHKXWa42m6NOa/421ab176b501f5bdae71290a8002545c/nba-logo_2x.png" alt="" height="400" width= "600"/>
     )
 }
 
